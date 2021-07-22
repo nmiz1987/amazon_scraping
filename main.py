4f6071ef17da9a12b5784d9cd4a582726f33fd53
Netanel Mizrahi <nmiz1987@gmail.com>
2021-07-23 00:44:00 +0300

    final

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ HEADERS = ({'User-Agent':
             'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
             'Accept-Language': 'en-US, en;q=0.5'})
 
-# send_url = os.environ['SEND_URL']
+send_url = os.environ['SEND_URL']
 
 print("Program starts...")
 
@@ -19,19 +19,20 @@ def check(title, url):
     # this part gets the price in dollars from amazon.com store
     try:
         price = float(soup.find(id='priceblock_ourprice').get_text().replace('$', '').replace(',', '').strip())
-        print(price)
     except Exception as err:
         price = ''
         print("Error info:", err)
-        
-    
+            
     msg = f"price: {price}$"
     print("--------------------> ",msg)
-    # requests.get(send_url + title + "\n"+msg +"\n"+ url)
+    requests.get(send_url + title + ":\n"+msg +"\n"+ url)
 
-check(title="500G SSD", url="https://www.amazon.com/dp/B08S8J19XJ?tag=camelproducts-20&linkCode=ogi&th=1&psc=1&language=en_US")
-# check(title="Sunglasses", url="https://www.amazon.com/Ray-Ban-Polarized-Rectangular-Sunglasses-Gradient/dp/B00NH9DWXI/ref=sr_1_3?dchild=1&keywords=RB4179&qid=1626975182&sr=8-3")
-# schedule.every().day.at("23:50").do(check)
-# while True:
-    # schedule.run_pending()
-    # time.sleep(1)
+
+
+
+schedule.every().day.at("10:00").do(check, "500G SSD", "https://www.amazon.com/dp/B08S8J19XJ?tag=camelproducts-20&linkCode=ogi&th=1&psc=1&language=en_US")
+schedule.every().day.at("10:00").do(check, "Sunglasses", "https://www.amazon.com/Ray-Ban-Polarized-Rectangular-Sunglasses-Gradient/dp/B00NH9DWXI/ref=sr_1_3?dchild=1&keywords=RB4179&qid=1626975182&sr=8-3")
+
+while True:
+    schedule.run_pending()
+    time.sleep(10)
