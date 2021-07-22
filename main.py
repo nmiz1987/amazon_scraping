9e7fdcbf4f43515e9a2689770b1c6eb55a702630
Netanel Mizrahi <nmiz1987@gmail.com>
2021-07-23 00:02:03 +0300

    final

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def check(title, url):
     # to prevent script from crashing when there isn't a price for the product
     try:
         print("-----------------------> find price")
+        time.sleep(2)
         print("***** ",soup.find(id='priceblock_ourprice'))
         price = float(soup.find(id='priceblock_ourprice').get_text().replace('.', '').replace('€', '').replace(',', '.').strip())
     except:
@@ -35,7 +36,7 @@ def check(title, url):
     # requests.get(send_url + title + "\n"+msg +"\n"+ url)
 
 check(title="500G SSD", url="https://www.amazon.com/dp/B08S8J19XJ?tag=camelproducts-20&linkCode=ogi&th=1&psc=1&language=en_US")
-check(title="Sunglasses", url="https://www.amazon.com/Ray-Ban-Polarized-Rectangular-Sunglasses-Gradient/dp/B00NH9DWXI/ref=sr_1_3?dchild=1&keywords=RB4179&qid=1626975182&sr=8-3")
+# check(title="Sunglasses", url="https://www.amazon.com/Ray-Ban-Polarized-Rectangular-Sunglasses-Gradient/dp/B00NH9DWXI/ref=sr_1_3?dchild=1&keywords=RB4179&qid=1626975182&sr=8-3")
 # schedule.every().day.at("23:50").do(check)
 # while True:
     # schedule.run_pending()
