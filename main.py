0cd127ded550a354615601ff418cdc4efd025a78
Netanel Mizrahi <nmiz1987@gmail.com>
2021-07-22 23:36:38 +0300

    final

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ HEADERS = ({'User-Agent':
             'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
             'Accept-Language': 'en-US, en;q=0.5'})
 
-send_url = environ['SEND_URL']
+send_url = os.environ['SEND_URL']
 
 print("Program starts...")
 
