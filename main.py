307b0e58d68c4a1c8203cd2cd9df6f61307fb6d5
Netanel Mizrahi <nmiz1987@gmail.com>
2021-07-22 23:43:12 +0300

    final

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ def check(title, url):
             price = float(soup.find(id='priceblock_ourprice').get_text().replace('$', '').replace(',', '').strip())
         except:
             price = ''
-    price_IL = float(price)*3.4
-    msg = f"price: {price}$, or {price_IL} NIS"
+    print(type(price))
+    msg = f"price: {price}$"
     requests.get(send_url + title + "\n"+msg +"\n"+ url)
 
 check(title="500G SSD", url="https://www.amazon.com/dp/B08S8J19XJ?tag=camelproducts-20&linkCode=ogi&th=1&psc=1&language=en_US")
