ec23bf3b353ea6a6da69a830742a45bd6396ca9a
Netanel Mizrahi <nmiz1987@gmail.com>
2021-07-22 21:35:50 +0300

    master

--- main.py
+++ main.py
@@ -5,18 +5,17 @@ import schedule
 import os
 
 
-url = "https://www.amazon.com/dp/B08S8J19XJ?tag=camelproducts-20&linkCode=ogi&th=1&psc=1&language=en_US"
 HEADERS = ({'User-Agent':
             'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
             'Accept-Language': 'en-US, en;q=0.5'})
 
 send_url = environ['SEND_URL']
 
+print("Program starts")
 
-def check():
+def check(title, url):
     r = requests.get(url, headers=HEADERS)
     soup = bs4.BeautifulSoup(r.content, features="lxml")
-    title = soup.find(id='productTitle').get_text().strip()
     # to prevent script from crashing when there isn't a price for the product
     try:
         price = float(soup.find(id='priceblock_ourprice').get_text().replace('.', '').replace('€', '').replace(',', '.').strip())
@@ -27,9 +26,10 @@ def check():
         except:
             price = ''
     msg = f"price: {price}$, or {price*3.4} NIS"
-    requests.get(send_url + "500G SSD" + "\n"+msg +"\n"+ url)
+    requests.get(send_url + title + "\n"+msg +"\n"+ url)
 
-check()
+check(title="500G SSD", url="https://www.amazon.com/dp/B08S8J19XJ?tag=camelproducts-20&linkCode=ogi&th=1&psc=1&language=en_US")
+check(title="משקפי שמש", url="https://www.amazon.com/Ray-Ban-Polarized-Rectangular-Sunglasses-Gradient/dp/B00NH9DWXI/ref=sr_1_3?dchild=1&keywords=RB4179&qid=1626975182&sr=8-3")
 # schedule.every().day.at("23:50").do(check)
 # while True:
     # schedule.run_pending()
