9a97ff87c8a0bf50efea1e4f4e6062e130c0acdb
Netanel Mizrahi <nmiz1987@gmail.com>
2021-07-23 00:17:23 +0300

    final

--- main.py
+++ main.py
@@ -9,27 +9,28 @@ HEADERS = ({'User-Agent':
             'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
             'Accept-Language': 'en-US, en;q=0.5'})
 
-# send_url = os.environ['SEND_URL']
+send_url = os.environ['SEND_URL']
 
 print("Program starts...")
 
 def check(title, url):
     r = requests.get(url, headers=HEADERS)
-    soup = bs4.BeautifulSoup(r.content, features="html5lib")
+    soup = bs4.BeautifulSoup(r.content, features="lxml")
     # to prevent script from crashing when there isn't a price for the product
     try:
+        print("-----------------------> find price")
         price = float(soup.find(id='priceblock_ourprice').get_text().replace('.', '').replace('€', '').replace(',', '.').strip())
     except:
         # this part gets the price in dollars from amazon.com store
         try:
             price = float(soup.find(id='priceblock_ourprice').get_text().replace('$', '').replace(',', '').strip())
-            print("3)",soup.find(id='priceblock_ourprice'))
         except Exception as err:
             price = ''
             print("Error info:", err)
         
     
     msg = f"price: {price}$"
+    print("--------------------> ",msg)
     # requests.get(send_url + title + "\n"+msg +"\n"+ url)
 
 check(title="500G SSD", url="https://www.amazon.com/dp/B08S8J19XJ?tag=camelproducts-20&linkCode=ogi&th=1&psc=1&language=en_US")
