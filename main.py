e9d66ef08f12233bb88f7d7c7adc4df3c8e15c1f
Netanel Mizrahi <nmiz1987@gmail.com>
2021-07-22 23:51:59 +0300

    final

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def check(title, url):
         except:
             price = 'problem'
     
-    msg = f"price: {price}$ \ {price*3.4} NIS"
+    msg = f"price: {price}$"
     requests.get(send_url + title + "\n"+msg +"\n"+ url)
 
 check(title="500G SSD", url="https://www.amazon.com/dp/B08S8J19XJ?tag=camelproducts-20&linkCode=ogi&th=1&psc=1&language=en_US")
