e27da8b69f84b94154fc5b6222e20959008aac90
Netanel Mizrahi <nmiz1987@gmail.com>
2021-07-24 21:04:46 +0300

    final

--- main.py
+++ main.py
@@ -9,13 +9,12 @@ HEADERS = ({'User-Agent':
             'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
             'Accept-Language': 'en-US, en;q=0.5'})
 
-send_url = os.environ['SEND_URL']
+# send_url = os.environ['SEND_URL']
 
 print("Program starts...")
 
 def check(title, url):
     r = requests.get(url, headers=HEADERS)
-    time.sleep(5)
     soup = bs4.BeautifulSoup(r.content, features="lxml")
     # this part gets the price in dollars from amazon.com store
     try:
@@ -24,29 +23,10 @@ def check(title, url):
     except Exception as err:
         price = ''
         print("Error info:", err)
-
-    # price1 = soup.find(id='priceblock_ourprice').get_text().replace('$', '').replace(',', '').strip()
-    # price2 = soup.find(id='priceblock_ourprice').get_text().replace('$', '').replace(',', '')
-    # price3 = soup.find(id='priceblock_ourprice').get_text().replace('$', '')
-    # price4 = soup.find(id='priceblock_ourprice').get_text()
-    # price5 = soup.find(id='priceblock_ourprice')    
+ 
     msg = f"price: {price}$"
     print("--------------------> ",msg)
-    # requests.get(send_url + title + ":\n"+msg +"\n"+ url)
-    # requests.get(send_url + price)
-    # requests.get(send_url + price1)
-    # requests.get(send_url + price2)
-    # requests.get(send_url + price3)
-    # requests.get(send_url + price4)
-    # requests.get(send_url + price5)
-    requests.get(send_url + r)
-
+    requests.get(send_url + title + ":\n"+msg +"\n"+ url)
 
 
 check("500G SSD", "https://www.amazon.com/dp/B08S8J19XJ?tag=camelproducts-20&linkCode=ogi&th=1&psc=1&language=en_US")
-# schedule.every().day.at("07:00").do(check, "500G SSD", "https://www.amazon.com/dp/B08S8J19XJ?tag=camelproducts-20&linkCode=ogi&th=1&psc=1&language=en_US")
-# schedule.every().day.at("07:00").do(check, "Sunglasses", "https://www.amazon.com/Ray-Ban-Polarized-Rectangular-Sunglasses-Gradient/dp/B00NH9DWXI/ref=sr_1_3?dchild=1&keywords=RB4179&qid=1626975182&sr=8-3")
-
-# while True:
-    # schedule.run_pending()
-    # time.sleep(10)
